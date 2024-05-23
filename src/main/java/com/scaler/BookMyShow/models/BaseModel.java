0bd680af347ac887376052d95ffdde4ee8128f1f
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-05-23 23:59:51 +0530

    Updated

--- src/main/java/com/scaler/BookMyShow/models/BaseModel.java
+++ src/main/java/com/scaler/BookMyShow/models/BaseModel.java
@@ -1,8 +1,10 @@
 package com.scaler.BookMyShow.models;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
@@ -10,7 +12,14 @@ import java.util.Date;
 @Setter
 public class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @CreatedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date updatedAt;
 }
