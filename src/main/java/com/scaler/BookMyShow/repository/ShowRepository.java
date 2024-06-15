3f9ef635dfd40fdf244f876da3da4ac705917b92
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-06-15 23:51:41 +0530

    Added show repository

---
+++ src/main/java/com/scaler/BookMyShow/repository/ShowRepository.java
@@ -0,0 +1,13 @@
+package com.scaler.BookMyShow.repository;
+
+import com.scaler.BookMyShow.models.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ShowRepository extends JpaRepository<Show, Long> {
+    @Override
+    Optional<Show> findById(Long showId);
+    @Override
+    Show save(Show show);
+}
