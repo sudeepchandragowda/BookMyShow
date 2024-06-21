4e32f03303d907c0cee00e3bb791c0710c2b7af4
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-06-22 04:29:07 +0530

    Created interface for TicketService with book, cancel and transfer ticket methods

---
+++ src/main/java/com/scaler/BookMyShow/services/TicketService.java
@@ -0,0 +1,11 @@
+package com.scaler.BookMyShow.services;
+
+import com.scaler.BookMyShow.models.Ticket;
+
+import java.util.List;
+
+public interface TicketService {
+    Ticket bookTicket(Long userId, List<Long> showSeatIds, Long showId);
+    Ticket cancelTicket(Long ticketId);
+    Ticket transferTicket(Long fromUserId, Long toUserId);
+}
