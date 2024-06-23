ab42abd923afb9fec748b017931fc2af36ad7687
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-06-24 00:00:58 +0530

    Updated controller by removing import of UserServiceImpl class

--- src/main/java/com/scaler/BookMyShow/controller/UserController.java
+++ src/main/java/com/scaler/BookMyShow/controller/UserController.java
@@ -5,7 +5,6 @@ import com.scaler.BookMyShow.dto.UserSignUpRequestDTO;
 import com.scaler.BookMyShow.dto.UserSignUpResponseDTO;
 import com.scaler.BookMyShow.models.Users;
 import com.scaler.BookMyShow.services.UserService;
-import com.scaler.BookMyShow.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
